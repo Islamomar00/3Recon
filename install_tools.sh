e8ac3a8f8d453d4a676d39db938974e2c02fe981
Eslam Omar <>
2023-08-04 02:15:02 +0300

    Add install script languages

--- install_tools.sh
+++ install_tools.sh
@@ -4,12 +4,24 @@
 mkidr tools
 cd tools
 
+# Install script languages
+
+apt-get install golang -Y
+apt-get install python3 -Y
+apt-get install python3-pip -Y
+# there is no more python-pip
+# sudo apt-get install python-pip
+apt-get install ruby -Y
+apt-get install screen -Y
+apt-get install git -Y
 
 # Install some tools
 apt-get install httpx -Y
 apt-get install dirsearch -Y
 apt-get install sqlmap -Y
-
+# there is no more pip, sho this is the same as below
+# pip install requests
+pip3 install requests
 # install assetfinder 
 wget https://github.com/tomnomnom/assetfinder/releases/download/v0.1.1/assetfinder-darwin-amd64-0.1.1.tgz
 # install httprope 
